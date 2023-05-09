dc08eab1fce94a2f72e98cd2d64f9b06ebcec69c
zeebroowskaa <125818706+zeebroowskaa@users.noreply.github.com>
2023-05-09 16:43:50 +0200

    test

--- Aplikacja-io/Home.aspx.cs
+++ Aplikacja-io/Home.aspx.cs
@@ -22,7 +22,7 @@ namespace Aplikacja_io
             {
                 if (pp.Id == 1)
                 {
-                    Label1.Text = pp.Nazwa;
+                    Label1.Text = "dziala";
                 }
             }
         }
